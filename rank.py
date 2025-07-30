6f881ec5dc77c1fb3f15c7f27eaf3d2a480b598a
ragil_prasath <ragulprasath5555@gmail.com>
2025-07-30 23:32:27 +0530

    modified

--- rank.py
+++ rank.py
@@ -1,14 +1,18 @@
 # Find the rank of the list 
-# Eg: [90,70,80,60,50] // output : [5,3,4,2,1]
-n=input("Enter the number :")
-n=n.split(",")
-m=[]
+# Eg: [90,70,80,60,50] => Output: [5,3,4,2,1]
+
+n = input("Enter the numbers separated by commas: ")
+n = n.split(",")
+m = []
 for i in range(len(n)):
-    n[i]=int(n[i])
-l=sorted(n)
+    n[i] = int(n[i])
+l = sorted(n)
+rank = {}
+for i in range(len(l)):
+    if l[i] not in rank:
+        rank[l[i]] = i + 1
 for i in range(len(n)):
-    for j in range(len(l)):
-        if n[i]==l[j]:
-            m.append(j+1)
+    m.append(rank[n[i]])
+
 print(n)
 print(m)
